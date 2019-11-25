83ea22c00f1cd6b84fd4e722bdbfd624d3959621
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-11-25 20:44:54 +0100

    Stable version

---
+++ files/input.js
@@ -0,0 +1,10 @@
+const { foo } = require('foo')
+const { foo2 } = require('foo2')
+const { foo3, foo4 } = require('foo5')
+
+const test = require('test')
+
+const {
+	foo3,
+	foo4
+} = require('foo5')
---
+++ traverse.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+function replacerCurly(match, p1, p2, p3, p4, p5m, offset, string) {
+	return ['import {', p2, '} from \'', p4, '\''].join('')
+}
+
+function replacerSingle(match, p1, p2, p3, p4, p5m, offset, string) {
+	return ['import ', p2, ' from \'', p4, '\''].join('')
+}
+
+function compose(a, b) {
+	return function(x) {
+		return a(b(x))
+	}
+}
+
+function parseSingle(fileStr) {
+	const reg = /(const )([a-z, 0-9]*)( = require\(')([a-z/0-9]*)('\))/g
+	return fileStr.replace(reg, replacerSingle)
+}
+
+function parseSpread(fileStr) {
+	const reg = /(const {)([a-z, 0-9]*)(} = require\(')([a-z/ 0-9]*)('\))/g
+	return fileStr.replace(reg, replacerCurly)
+}
+
+const parseAll = compose(parseSpread, parseSingle)
+
+function rewrite(err, file) {
+	fs.writeFile('files/output.js', parseAll(file), () => {})
+}
+fs.readFile('files/input.js', 'utf8', rewrite)
